ebea6bdd1a7804185972e7dcbd2d727b94d52b38
yogi.s75 <yogi.s75@gmail.com>
2022-07-03 22:57:57 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  let joke;
+  const inputHandler = (e) => {
+    e.preventDefault();
+    joke = e.target.value;
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Joke Generator</h2>
+      <form>
+        <p>Enter your name!</p>
+        <input type="text"></input>
+        <button type="submit" onClick={inputHandler}>
+          Generate Joke
+        </button>
+      </form>
     </div>
   );
 }
