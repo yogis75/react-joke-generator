aa2c034482303657d8fe63c3b8d75b16d0eba4f7
yogi.s75 <yogi.s75@gmail.com>
2022-07-03 22:59:07 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 function App() {
-  let joke;
-  const inputHandler = (e) => {
-    e.preventDefault();
-    joke = e.target.value;
-  };
   return (
     <div className="App">
       <h2>Joke Generator</h2>
       <form>
         <p>Enter your name!</p>
         <input type="text"></input>
-        <button type="submit" onClick={inputHandler}>
-          Generate Joke
-        </button>
+        <button type="submit">Generate Joke</button>
       </form>
     </div>
   );
