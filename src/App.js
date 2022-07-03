88f137ea30047d95f9e43d78545669703dbb2f1c
yogi.s75 <yogi.s75@gmail.com>
2022-07-03 23:12:05 +0530

    css again

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ function App() {
   return (
     <div className="App">
       <h2>Joke Generator</h2>
-      <form>
-        <p>Enter your name!</p>
-        <input type="text"></input>
-        <button type="submit">Generate Joke</button>
-      </form>
+      <p>Enter your name!</p>
+      <input type="text"></input>
+      <button type="submit">Generate Joke</button>
     </div>
   );
 }
