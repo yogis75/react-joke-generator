a81d224ee009fbee8e408ea9b7122342146929a2
yogi.s75 <yogi.s75@gmail.com>
2022-07-04 23:55:36 +0530

    useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const API = `https://v2.jokeapi.dev/joke/Any?type=single`;
+
+  const generateJoke = () => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((data) => setJoke(data.joke));
+  };
+
+  useEffect(() => {
+    generateJoke();
+  }, []);
+
+  const [joke, setJoke] = useState("");
+
   return (
     <div className="App">
       <h2>Joke Generator</h2>
-      <p>Enter your name!</p>
-      <input type="text"></input>
-      <button type="submit">Generate Joke</button>
+      {/* <p>Enter your name!</p>
+      <input type="text"></input> */}
+      <p>{joke}</p>
+      <button onClick={generateJoke} type="submit">
+        Generate Joke
+      </button>
     </div>
   );
 }
