3b39704eed64132f97957fe046d5d608ee354d84
yogi.s75 <yogi.s75@gmail.com>
2022-07-08 00:00:39 +0530

    Filtered Jokes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const API = `https://v2.jokeapi.dev/joke/Any?type=single`;
+  //State
+  const [joke, setJoke] = useState("Here will be the Joke!");
+  const [typeOfJoke, setTypeOfJoke] = useState("Any");
+  const [filter, setFilter] = useState("");
+  const API = `https://v2.jokeapi.dev/joke/${typeOfJoke}?type=single`;
+
+  //Functions
+  const filterHandler = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const typeHandler = () => {
+    switch (filter) {
+      case "Programming":
+        setTypeOfJoke("Programming");
+        break;
+      case "Misc":
+        setTypeOfJoke("Misc");
+        break;
+      case "Pun":
+        setTypeOfJoke("Pun");
+        break;
+      default:
+        setTypeOfJoke("Any");
+        break;
+    }
+  };
 
   const generateJoke = () => {
     fetch(API)
@@ -10,17 +36,21 @@ function App() {
   };
 
   useEffect(() => {
-    generateJoke();
-  }, []);
-
-  const [joke, setJoke] = useState("");
+    typeHandler();
+  }, [filter]);
 
   return (
     <div className="App">
       <h2>Joke Generator</h2>
-      {/* <p>Enter your name!</p>
-      <input type="text"></input> */}
+      {/* <input type="text" placeholder="Enter your name" /> */}
+
       <p>{joke}</p>
+      <select onChange={filterHandler}>
+        <option>Any</option>
+        <option>Programming</option>
+        <option>Misc</option>
+        <option>Pun</option>
+      </select>
       <button onClick={generateJoke} type="submit">
         Generate Joke
       </button>
